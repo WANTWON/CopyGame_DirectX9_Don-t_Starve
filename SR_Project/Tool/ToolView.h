6bbcc3470b58f435962c7fda072a2e734b6012ed
WANTWON <106663427+WANTWON@users.noreply.github.com>
2022-08-22 13:09:07 +0900

    Add MFC

---
+++ SR_Project/Tool/ToolView.h
@@ -0,0 +1,49 @@
+
+// ToolView.h : CToolView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CToolView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CToolView();
+	DECLARE_DYNCREATE(CToolView)
+
+// 특성입니다.
+public:
+	CToolDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CToolView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // ToolView.cpp의 디버그 버전
+inline CToolDoc* CToolView::GetDocument() const
+   { return reinterpret_cast<CToolDoc*>(m_pDocument); }
+#endif
+
