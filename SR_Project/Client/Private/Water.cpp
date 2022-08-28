093ddd4299e26105993c4c5b2bbde2656b0782db
WANTWON <106663427+WANTWON@users.noreply.github.com>
2022-08-28 15:00:59 +0900

    Water

---
+++ SR_Project/Client/Private/Water.cpp
@@ -0,0 +1,231 @@
+#include "stdafx.h"
+#include "..\Public\Water.h"
+#include "GameInstance.h"
+
+CWater::CWater(LPDIRECT3DDEVICE9 pGraphic_Device)
+	: CGameObject(pGraphic_Device)
+{
+}
+
+CWater::CWater(const CWater & rhs)
+	: CGameObject(rhs)
+{
+}
+
+HRESULT CWater::Initialize_Prototype()
+{
+	if (FAILED(__super::Initialize_Prototype()))
+		return E_FAIL;
+
+
+	return S_OK;
+}
+
+HRESULT CWater::Initialize(void* pArg)
+{
+	if (FAILED(__super::Initialize(pArg)))
+		return E_FAIL;
+
+	if (FAILED(SetUp_Components(pArg)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CWater::Initialize_Load(const _tchar * VIBufferTag, void * pArg)
+{
+	if (FAILED(SetUp_Components(VIBufferTag, pArg)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+int CWater::Tick(_float fTimeDelta)
+{
+	__super::Tick(fTimeDelta);
+
+	return OBJ_NOEVENT;
+}
+
+void CWater::Late_Tick(_float fTimeDelta)
+{
+	__super::Late_Tick(fTimeDelta);
+
+	if (nullptr != m_pRendererCom)
+		m_pRendererCom->Add_RenderGroup(CRenderer::RENDER_NONALPHABLEND, this);
+}
+
+HRESULT CWater::Render()
+{
+	if (FAILED(__super::Render()))
+		return E_FAIL;
+
+	if (FAILED(m_pTransformCom->Bind_OnGraphicDev()))
+		return E_FAIL;
+
+	if (FAILED(m_pTextureCom->Bind_OnGraphicDev(m_pVIBufferCom->GetTerrainDesc().m_iTextureNum)))
+		return E_FAIL;
+
+	if (FAILED(SetUp_RenderState()))
+		return E_FAIL;
+
+	m_pVIBufferCom->Render();
+
+	if (FAILED(Release_RenderState()))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CWater::SetUp_Components(void* pArg)
+{
+	/* For.Com_Renderer */
+	if (FAILED(__super::Add_Components(TEXT("Com_Renderer"), LEVEL_STATIC, TEXT("Prototype_Component_Renderer"), (CComponent**)&m_pRendererCom)))
+		return E_FAIL;
+
+	/* For.Com_Texture */
+	if (FAILED(__super::Add_Components(TEXT("Com_Texture"), LEVEL_STATIC, TEXT("Prototype_Component_Texture_Terrain"), (CComponent**)&m_pTextureCom)))
+		return E_FAIL;
+
+
+	m_TerrainDesc.m_iNumVerticesX = 50;
+	m_TerrainDesc.m_iNumVerticesZ = 50;
+	m_TerrainDesc.m_fTextureSize = 5.f;
+	m_TerrainDesc.m_fSizeX = 1;
+	m_TerrainDesc.m_fSizeZ = 1;
+	m_TerrainDesc.m_iTextureNum = 17;
+
+	/* For.Com_VIBuffer */
+	if (FAILED(__super::Add_Components(TEXT("Com_VIBuffer"), LEVEL_GAMEPLAY, TEXT("Prototype_Component_VIBuffer_Terrain"), (CComponent**)&m_pVIBufferCom, &m_TerrainDesc)))
+		return E_FAIL;
+
+
+	/* For.Com_Transform */
+	CTransform::TRANSFORMDESC		TransformDesc;
+	ZeroMemory(&TransformDesc, sizeof(CTransform::TRANSFORMDESC));
+
+	TransformDesc.fSpeedPerSec = 5.f;
+	TransformDesc.fRotationPerSec = D3DXToRadian(90.0f);
+	TransformDesc.InitPos = *(_float3*)pArg;
+
+	if (FAILED(__super::Add_Components(TEXT("Com_Transform"), LEVEL_STATIC, TEXT("Prototype_Component_Transform"), (CComponent**)&m_pTransformCom, &TransformDesc)))
+		return E_FAIL;
+
+
+	return S_OK;
+}
+
+HRESULT CWater::SetUp_Components(const _tchar * VIBufferTag, void * pArg)
+{
+	/* For.Com_Renderer */
+	if (FAILED(__super::Add_Components(TEXT("Com_Renderer"), LEVEL_STATIC, TEXT("Prototype_Component_Renderer"), (CComponent**)&m_pRendererCom)))
+		return E_FAIL;
+
+	/* For.Com_Texture */
+	if (FAILED(__super::Add_Components(TEXT("Com_Texture"), LEVEL_STATIC, TEXT("Prototype_Component_Texture_Terrain"), (CComponent**)&m_pTextureCom)))
+		return E_FAIL;
+
+	/* For.Com_VIBuffer */
+	if (FAILED(__super::Add_Components(TEXT("Com_VIBuffer"), LEVEL_STATIC, VIBufferTag, (CComponent**)&m_pVIBufferCom, pArg)))
+		return E_FAIL;
+
+
+	/* For.Com_Transform */
+	CTransform::TRANSFORMDESC		TransformDesc;
+	ZeroMemory(&TransformDesc, sizeof(CTransform::TRANSFORMDESC));
+
+	TransformDesc.fSpeedPerSec = 5.f;
+	TransformDesc.fRotationPerSec = D3DXToRadian(90.0f);
+	if (pArg != nullptr)
+		TransformDesc.InitPos = *(_float3*)pArg;
+
+	if (FAILED(__super::Add_Components(TEXT("Com_Transform"), LEVEL_STATIC, TEXT("Prototype_Component_Transform"), (CComponent**)&m_pTransformCom, &TransformDesc)))
+		return E_FAIL;
+
+
+	return S_OK;
+}
+
+HRESULT CWater::SetUp_RenderState()
+{
+	if (nullptr == m_pGraphic_Device)
+		return E_FAIL;
+
+	//m_pGraphic_Device->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
+
+	return S_OK;
+}
+
+HRESULT CWater::SetUp_SamplerState()
+{
+
+	if (nullptr == m_pGraphic_Device)
+		return E_FAIL;
+
+	m_pGraphic_Device->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
+	m_pGraphic_Device->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
+	m_pGraphic_Device->SetSamplerState(0, D3DSAMP_MIPFILTER, D3DTEXF_LINEAR);
+
+	return S_OK;
+}
+
+HRESULT CWater::Release_RenderState()
+{
+	//m_pGraphic_Device->SetRenderState(D3DRS_FILLMODE, D3DFILL_SOLID);
+
+	m_pGraphic_Device->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_NONE);
+	m_pGraphic_Device->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_NONE);
+	m_pGraphic_Device->SetSamplerState(0, D3DSAMP_MIPFILTER, D3DTEXF_NONE);
+
+	return S_OK;
+}
+
+
+CWater * CWater::Create(LPDIRECT3DDEVICE9 pGraphic_Device)
+{
+	CWater*	pInstance = new CWater(pGraphic_Device);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		ERR_MSG(TEXT("Failed to Created : CTerrain"));
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+CGameObject * CWater::Clone(void* pArg)
+{
+	CWater*	pInstance = new CWater(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		ERR_MSG(TEXT("Failed to Cloned : CTerrain"));
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+CGameObject * CWater::Clone_Load(const _tchar * VIBufferTag, void * pArg)
+{
+	CWater*	pInstance = new CWater(*this);
+
+	if (FAILED(pInstance->Initialize_Load(VIBufferTag, pArg)))
+	{
+		ERR_MSG(TEXT("Failed to Cloned : CTerrain"));
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CWater::Free()
+{
+	__super::Free();
+
+	Safe_Release(m_pTransformCom);
+	Safe_Release(m_pVIBufferCom);
+	Safe_Release(m_pRendererCom);
+	Safe_Release(m_pTextureCom);
+}
