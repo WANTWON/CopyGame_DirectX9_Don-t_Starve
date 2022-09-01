253d94f6c60ca6dc97e17851e92dd46e2f118192
gosomdochimon <dnwlsthsus@naver.com>
2022-09-02 06:17:25 +0900

    Revert "Merge branch 'Main_Test' into Woojin2"
    This reverts commit ef8ad3a2978af5beac83707228ed1d78149295a8, reversing
    changes made to e525d0208e53a8eb70d729f6a24fc47d6f7f052e.

--- SR_Project/Client/Public/Level_GamePlay.h
+++ SR_Project/Client/Public/Level_GamePlay.h
@@ -20,7 +20,6 @@ public:
 	HRESULT Ready_Layer_BackGround(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Monster(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Object(const _tchar* pLayerTag);
-	HRESULT Ready_Layer_Portal(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Player(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Camera(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_MainInventory(const _tchar* pLayerTag);
