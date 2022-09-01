c5674034025703264aa1ad8df81a05ba16408414
WANTWON <106663427+WANTWON@users.noreply.github.com>
2022-09-02 05:48:26 +0900

    Solved Error

--- SR_Project/Client/Public/Level_GamePlay.h
+++ SR_Project/Client/Public/Level_GamePlay.h
@@ -20,6 +20,7 @@ public:
 	HRESULT Ready_Layer_BackGround(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Monster(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Object(const _tchar* pLayerTag);
+	HRESULT Ready_Layer_Portal(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Player(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_Camera(const _tchar* pLayerTag);
 	HRESULT Ready_Layer_MainInventory(const _tchar* pLayerTag);
