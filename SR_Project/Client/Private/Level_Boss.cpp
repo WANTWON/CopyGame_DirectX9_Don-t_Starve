47584b9b4cac30537879db91bd40742906758cfe
Gioi Giuseppe <giuseppe.gioi.1997@gmail.com>
2022-09-13 23:00:30 +0900

    Boarrior moved to Boss Stage.

--- SR_Project/Client/Private/Level_Boss.cpp
+++ SR_Project/Client/Private/Level_Boss.cpp
@@ -26,8 +26,8 @@ HRESULT CLevel_Boss::Initialize()
 	if (FAILED(Ready_Layer_BackGround(TEXT("Layer_Terrain"))))
 		return E_FAIL;
 
-	/*if (FAILED(Ready_Layer_Monster(TEXT("Layer_Monster"))))
-		return E_FAIL;*/
+	if (FAILED(Ready_Layer_Monster(TEXT("Layer_Monster"))))
+		return E_FAIL;
 
 	if (FAILED(Ready_Layer_Object(TEXT("Layer_Object"))))
 		return E_FAIL;
@@ -87,8 +87,8 @@ HRESULT CLevel_Boss::Ready_Layer_Monster(const _tchar * pLayerTag)
 	CGameInstance*			pGameInstance = CGameInstance::Get_Instance();
 	Safe_AddRef(pGameInstance);
 
-	/*if (FAILED(pGameInstance->Add_GameObject(TEXT("Prototype_GameObject_Boarrior"), LEVEL_GAMEPLAY, pLayerTag, _float3(0.f, 0.f, 0.f))))
-		return E_FAIL;*/
+	if (FAILED(pGameInstance->Add_GameObject(TEXT("Prototype_GameObject_Boarrior"), LEVEL_BOSS, pLayerTag, _float3(19.f, 0.f, 26.f))))
+		return E_FAIL;
 
 	Safe_Release(pGameInstance);
 
